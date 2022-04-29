4c82042e35d7b4633e0dcedd5933f24505772512
IgorKilipenko <igorkili@mail.ru>
2022-04-29 21:24:24 +0700

    mig to transmit at buffer

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,10 @@
 HardwareSerial *RTCM{&Serial1};
 HardwareSerial *Receiver{&Serial2};
 
-
-
 std::thread receiverThread;
 
 using UartBuffer = Queue<std::vector<uint8_t>>;
-UartBuffer queue_buffer{500};
+// UartBuffer queue_buffer{500};
 
 const std::vector<uint8_t> readReceiverData()
 {
@@ -26,15 +24,13 @@ const std::vector<uint8_t> readReceiverData()
         }
         if (len > 0)
         {
-            //queue_buffer.push(buffer);
+            // queue_buffer.push(buffer);
             log_d("data count %d\n", buffer.size());
             return buffer;
         }
     }
     return std::vector<uint8_t>(0);
 }
-
-
 
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
@@ -50,7 +46,7 @@ void setup()
 {
     Serial.begin(BAUD_SERIAL);
 
-    Receiver->begin(BAUND_RECEIVER, SERIAL_8N1, RXD2, TXD2);
+    Receiver->begin(BAUND_RECEIVER, SERIAL_8N1, RXD2, TXD2, false, 20000UL, static_cast<uint8_t>(SERIAL_SIZE_RX));
     delay(1000);
     unsigned long detectedBaudRate = Receiver->baudRate();
     if (detectedBaudRate)
@@ -58,46 +54,79 @@ void setup()
     else
     {
         log_w("Receiver baudrate not detected");
-        Receiver->begin(BAUD_SERIAL, SERIAL_8N1, RXD2, TXD2);
+        Receiver->begin(BAUD_SERIAL, SERIAL_8N1, RXD2, TXD2, false, 20000UL, SERIAL_SIZE_RX);
     }
 
     RTCM->begin(38400, SERIAL_8N1, RXD1, TXD1);
 
-    Receiver->setRxBufferSize(SERIAL_SIZE_RX);
+    // Receiver->setRxBufferSize(SERIAL_SIZE_RX);
 
     SerialBT.begin("ESP32test"); // Bluetooth device name
     Serial.println("The device started, now you can pair it with bluetooth!");
 }
-std::vector<uint8_t> buffer(1024*2);
+std::vector<uint8_t> buffer(SERIAL_SIZE_RX * 10);
 int tryCount = 0;
 void loop()
 {
-    if (SerialBT.hasClient()) {
+    if (SerialBT.hasClient())
+    {
         const size_t bufferSize = buffer.size();
-        if (bufferSize > 256 || tryCount > 3) {
-            for (int i = 0; i < bufferSize; i++) {
-                SerialBT.write(buffer[i]);
+        if (bufferSize >= SERIAL_SIZE_RX * 5 || tryCount > 10)
+        {
+            log_d("BufferSize = %lu, tryCount = %d", bufferSize, tryCount);
+            int partSize = bufferSize - SerialBT.write(buffer.data(), bufferSize);
+            if (partSize <= 0) {
+                tryCount = 0;
+                buffer.clear();
             }
-            buffer.clear();
-            tryCount = 0;
-        } else {
-            tryCount++;
+            else {
+                log_d("BT Buffer overflow = %lu, tryCount = %d", partSize, tryCount);
+                std::vector<uint8_t> bufferOld(partSize);
+                const size_t oldBufferSize = buffer.size();
+                for (int i = bufferSize-1; i < oldBufferSize + bufferSize; i++)
+                {
+                    bufferOld.push_back(buffer[i]);
+                }
+
+                buffer.clear();
+                for (int i=0; i < oldBufferSize; i++) {
+                    buffer.push_back(bufferOld[i]);
+                }
+            }
+            delay(10);
+        }
+        else
+        {
+            if (bufferSize > 0)
+            {
+                tryCount++;
+            }
         }
 
         const int len = Receiver->available();
         if (len)
         {
-            //SerialBT.write(Serial.read());
+            // SerialBT.write(Serial.read());
             uint8_t bytes[len];
             const size_t sizeRes = Receiver->readBytes(bytes, len);
-            for (int i = 0; i < sizeRes; i++) {
+            for (int i = 0; i < sizeRes; i++)
+            {
                 buffer.push_back(bytes[i]);
             }
         }
         if (SerialBT.available())
         {
             Serial.write(SerialBT.read());
+            delay(10);
         }
     }
-    delay(10);
+    else
+    {
+        if (buffer.size() > 0)
+        {
+            buffer.clear();
+            tryCount = 0;
+        }
+    }
+    delay(1);
 }
