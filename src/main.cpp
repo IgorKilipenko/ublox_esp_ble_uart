955a172150174af0626ddf7dd49610c780fe96cb
IgorKilipenko <igorkili@mail.ru>
2022-05-01 04:12:48 +0700

    nfix onRecive callback next iter (nofix work fine)

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,6 @@
 HardwareSerial *RTCM{&Serial1};
 HardwareSerial *Receiver{&Serial2};
 
-
 const std::vector<uint8_t> readReceiverData()
 {
     int count = 0;
@@ -42,58 +41,63 @@ const size_t MAX_BUFFER_SIZE = 1024 * 1;
 std::vector<uint8_t> buffer(MAX_BUFFER_SIZE * 2);
 int tryCount = 0;
 
-void receiveFnc() {
+void receiveFnc()
+{
     log_d("Received UART Data. Available For Reading = %u\n", Receiver->available());
-    while(Receiver->available()) Receiver->read(); // Discard received data.
+    while (Receiver->available())
+        Receiver->read(); // Discard received data.
 }
 
-void receiveErrorFnc(hardwareSerial_error_t error){
+void receiveErrorFnc(hardwareSerial_error_t error)
+{
     log_e("UART Reception Error: ");
     bool bufferOverflow = false;
-    switch(error) {
-        case UART_BREAK_ERROR:
-            log_e("UART_BREAK_ERROR\n");
-            break;
-        case UART_BUFFER_FULL_ERROR:
-            log_e("UART_BUFFER_FULL_ERROR\n");
-            bufferOverflow = true;
-            break;
-        case UART_FIFO_OVF_ERROR:
-            log_e("UART_FIFO_OVF_ERROR\n");
-            bufferOverflow = true;
-            break;
-        case UART_FRAME_ERROR:
-            log_e("UART_FRAME_ERROR\n");
-            break;
-        case UART_PARITY_ERROR:
-            log_e("UART_PARITY_ERROR\n");
-            break;
+    switch (error)
+    {
+    case UART_BREAK_ERROR:
+        log_e("UART_BREAK_ERROR\n");
+        break;
+    case UART_BUFFER_FULL_ERROR:
+        log_e("UART_BUFFER_FULL_ERROR\n");
+        bufferOverflow = true;
+        break;
+    case UART_FIFO_OVF_ERROR:
+        log_e("UART_FIFO_OVF_ERROR\n");
+        bufferOverflow = true;
+        break;
+    case UART_FRAME_ERROR:
+        log_e("UART_FRAME_ERROR\n");
+        break;
+    case UART_PARITY_ERROR:
+        log_e("UART_PARITY_ERROR\n");
+        break;
     }
-    if (bufferOverflow) {
+    if (bufferOverflow)
+    {
         log_w("Clear buffers");
-        while(Receiver->available()) Receiver->read();
+        while (Receiver->available())
+            Receiver->read();
         buffer.clear();
     }
-    
 }
-
-
 
 void onGnssReceiveCb()
 {
-    if (!SerialBT.hasClient()) {
-        while(Receiver->available()) Receiver->read();
+    if (!SerialBT.hasClient())
+    {
+        while (Receiver->available())
+            Receiver->read();
         return;
     }
     const int len = Receiver->available();
     if (len)
     {
-        //uint8_t bytes[len];
-        //const size_t readSize = Receiver->readBytes(bytes, len);
+        // uint8_t bytes[len];
+        // const size_t readSize = Receiver->readBytes(bytes, len);
 
-        //const size_t writeCount = SerialBT.write(bytes, readSize);
-        //log_d("[ %lu ] bytes from GNSS[incom : %lu bytes] sent to BT SERIAL.\n", writeCount, len);
-        //delay(1);
+        // const size_t writeCount = SerialBT.write(bytes, readSize);
+        // log_d("[ %lu ] bytes from GNSS[incom : %lu bytes] sent to BT SERIAL.\n", writeCount, len);
+        // delay(1);
 
         /*while(Receiver->available()) SerialBT.write(Receiver->read());
         delay(10);*/
@@ -101,8 +105,8 @@ void onGnssReceiveCb()
             buffer.push_back(bytes[i]);
         }*/
 
-        while(Receiver->available()) buffer.push_back(Receiver->read());
-        
+        while (Receiver->available())
+            buffer.push_back(Receiver->read());
     }
 }
 
@@ -127,29 +131,29 @@ void setup()
 {
     Serial.begin(BAUD_SERIAL);
 
-    Receiver->setRxBufferSize(1024*4);
-    Receiver->onReceive(onGnssReceiveCb, true); 
-    Receiver->onReceiveError(receiveErrorFnc);
-    //Receiver->setRxTimeout(1);
+    SerialBT.begin("ESP32test"); // Bluetooth device name
+    Serial.println("The device started, now you can pair it with bluetooth!");
 
-    Receiver->begin(BAUND_RECEIVER, SERIAL_8N1, RXD2, TXD2/*, false, 20000UL, static_cast<uint8_t>(SERIAL_SIZE_RX)*/);
-    //delay(500);
+    Receiver->setRxBufferSize(1024 * 4);
+    Receiver->onReceive(onGnssReceiveCb, true);
+    Receiver->onReceiveError(receiveErrorFnc);
+    // Receiver->setRxTimeout(1);
+
+    Receiver->begin(BAUND_RECEIVER, SERIAL_8N1, RXD2, TXD2 /*, false, 20000UL, static_cast<uint8_t>(SERIAL_SIZE_RX)*/);
+    // delay(500);
     unsigned long detectedBaudRate = Receiver->baudRate();
     if (detectedBaudRate)
         log_d("Receiver uart baudrate detecyed -> [ %lu ]", detectedBaudRate);
     else
     {
         log_w("Receiver baudrate not detected");
-        Receiver->begin(BAUD_SERIAL, SERIAL_8N1, RXD2, TXD2/*, false, 20000UL, SERIAL_SIZE_RX*/);
+        Receiver->begin(BAUD_SERIAL, SERIAL_8N1, RXD2, TXD2 /*, false, 20000UL, SERIAL_SIZE_RX*/);
     }
     // Receiver->setHwFlowCtrlMode((uint8_t)3U, SERIAL_SIZE_RX);
 
     RTCM->begin(38400, SERIAL_8N1, RXD1, TXD1);
 
     // Receiver->setRxBufferSize(SERIAL_SIZE_RX);
-
-    SerialBT.begin("ESP32test"); // Bluetooth device name
-    Serial.println("The device started, now you can pair it with bluetooth!");
 
     /*if (Receiver != NULL)
     {
@@ -161,29 +165,21 @@ void setup()
     }*/
 }
 
-
-
 void loop()
 {
-    
     if (SerialBT.hasClient())
     {
         const size_t bufferSize = buffer.size();
-        if ((bufferSize >= MAX_BUFFER_SIZE - SERIAL_SIZE_RX) || (bufferSize > 0 && tryCount > 10))
+        if (bufferSize > 0)
         {
             // log_d("BufferSize = %lu, tryCount = %d", bufferSize, tryCount);
-            const size_t writeCount = SerialBT.write(buffer.data(), bufferSize);
+            const size_t writeCount = SerialBT.write(buffer.data(), min(bufferSize, (size_t)1024 * 2 * 8));
             log_d("Write %lu bytes from %lu", writeCount, bufferSize);
             int partSize = bufferSize - writeCount;
-            if (partSize <= 0)
+            if (partSize > 0)
             {
-                tryCount = 0;
-                buffer.clear();
-            }
-            else
-            {
-                log_d("WARN BT Buffer overflow = %lu, tryCount = %d", partSize, tryCount);
-                std::vector<uint8_t> bufferOld(partSize);
+                log_w("WARN BT Buffer overflow = %lu, Total buffer size = %lu\n", partSize, bufferSize);
+                /*std::vector<uint8_t> bufferOld(partSize);
                 for (int i = partSize - 1; i < bufferSize; i++)
                 {
                     bufferOld.push_back(buffer[i]);
@@ -194,9 +190,14 @@ void loop()
                 for (int i = 0; i < oldBufferSize; i++)
                 {
                     buffer.push_back(bufferOld[i]);
-                }
+                }*/
             }
-            if (writeCount >= MAX_BUFFER_SIZE - SERIAL_SIZE_RX)
+
+            tryCount = 0;
+            buffer.clear();
+
+            
+            if (writeCount >= MAX_BUFFER_SIZE * 8)
             {
                 delay(10);
                 SerialBT.flush();
